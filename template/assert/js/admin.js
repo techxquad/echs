4fb6a99e77bb7e7c801daadb1043c1f8e4bb53ac
Sudhanshu <sudhanshugolu2003@gmail.com>
2024-08-19 17:47:44 +0530

    dashboard ui

---
+++ template/assert/js/admin.js
@@ -0,0 +1,10 @@
+// Hide spinner after the page is loaded
+window.addEventListener('load', function () {
+    document.getElementById('loadingSpinner').classList.add('hidden');
+});
+
+// Sidebar toggle for mobile screens
+document.querySelector('.navbar-toggler').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebarMenu');
+    sidebar.classList.toggle('active');
+});
